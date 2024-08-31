69979c3c98c9e440c67a7342b6ca6693d59d6785
ayushplanscout <ayushpandey332@gmail.com>
2024-08-31 21:31:30 +0530

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router();
+
+const userRoutes = require('./user')
+const authRoutes = require('./auth')
+
+userRoutes(router);
+authRoutes(router);
+
+module.exports = router
